2339dac855fa7492283659c6f77c5ddf5e781c65
vipya <vipya@smile.fr>
2016-10-27 01:39:58 +0300

    category controller

---
+++ src/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Category controller
+ *
+ * @category controller
+ * @author   Vitaliy Pyatin <mail.pyvil@gmail.com>
+ */
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
+
+class CategoryController extends Controller
+{
+    /**
+     * Category create action
+     *
+     * @return void
+     */
+    public function create()
+    {
+        
+    }
+}
