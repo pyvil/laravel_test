c5ea52820b5fa73eba15796ffea27d88de8fd320
vipya <vipya@smile.fr>
2016-10-31 02:34:27 +0200

    migrations

--- src/database/migrations/2016_10_25_225704_task_category.php
+++ src/database/migrations/2016_10_25_225704_task_category.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * task_category connection table migration
+ *
+ * @category migration
+ * @author   Vyialiy Pyatin <mail.pyvil@gmail.com>
+ */
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
